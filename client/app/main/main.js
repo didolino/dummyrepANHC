a9ef6ed27447cc33aaeedc045ef7f78ccaf2fee6
didolino <anhc1696@aubih.edu>
2016-03-01 19:58:35 +0100

    First commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('ajdinHalilcevicFullstackApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
